eafd176ea2bda78558a5b8d4d6d5936655512827
Rok Černič <rok.cernic@gmail.com>
2017-06-11 12:54:59 +0200

    Blinds update driver sleep

--- blinds/blinds.ino
+++ blinds/blinds.ino
@@ -6,10 +6,11 @@
 #include <WiFiUdp.h>
 
 // Motor config
-#define DISTANCE            3200
+#define DISTANCE            30000
 #define MOTOR_PIN_STEP      D3
 #define MOTOR_PIN_DIRECTION D4
-#define BUTTON_PIN          D5
+#define MOTOR_PIN_SLEEP     D5
+#define BUTTON_PIN          D7
 
 // Specify times
 String open_blinds_time = "07:00:00";
@@ -31,12 +32,13 @@ void setup() {
 
   pinMode(MOTOR_PIN_STEP, OUTPUT);     
   pinMode(MOTOR_PIN_DIRECTION, OUTPUT);
+  pinMode(MOTOR_PIN_SLEEP, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   
   digitalWrite(MOTOR_PIN_STEP, LOW);
   digitalWrite(MOTOR_PIN_DIRECTION, LOW);
+  digitalWrite(MOTOR_PIN_SLEEP, LOW);
   digitalWrite(BUTTON_PIN, LOW);
-  
   
   WiFi.begin(ssid, password);
 
@@ -52,37 +54,36 @@ void loop() {
   timeClient.update();
   Serial.println(timeClient.getFormattedTime());
 
-  if (
-      (
-        (open_blinds_time == timeClient.getFormattedTime() && is_blind_open == false) 
-        || 
-        (close_blinds_time == timeClient.getFormattedTime() && is_blind_open == true) 
-        || 
-        digitalRead(BUTTON_PIN) == LOW
-      )
-      &&
-      stepping == false
-     ) {
-    stepping = true;
-  }
+  if (((open_blinds_time == timeClient.getFormattedTime() && is_blind_open == false) || 
+       (close_blinds_time == timeClient.getFormattedTime() && is_blind_open == true) || 
+        digitalRead(BUTTON_PIN) == LOW))
+  {
+    // Wake up driver
+    digitalWrite(MOTOR_PIN_SLEEP, HIGH);
 
-  if (is_blind_open == true) {
-    digitalWrite(MOTOR_PIN_DIRECTION, HIGH);
-  } else {
-    digitalWrite(MOTOR_PIN_DIRECTION, LOW);
-  }
-
-  if (stepping == true) {
-    step(1000);
-
-    step_counter = step_counter + 1;
-
-    if (step_counter == DISTANCE)
+    // Check direction
+    if (is_blind_open == true)
     {
-      step_counter = 0;
-      stepping = false;
-      is_blind_open = !is_blind_open;
+      digitalWrite(MOTOR_PIN_DIRECTION, HIGH);
     }
+    else
+    {
+      digitalWrite(MOTOR_PIN_DIRECTION, LOW);
+    }
+
+    // Turn motor
+    // TODO: Make non blocking
+    while (step_counter != DISTANCE)
+    {
+      step(200);
+      yield();
+      step_counter = step_counter + 1;
+    }
+
+    // Reset and sleep
+    is_blind_open = !is_blind_open;
+    step_counter = 0;
+    digitalWrite(MOTOR_PIN_SLEEP, LOW);
   }
 }
 
